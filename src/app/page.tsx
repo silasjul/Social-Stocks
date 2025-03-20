e62c44037d34abdbff46a4d9b168830e1a4110c6
silasjul <silaskierstein@gmail.com>
2025-03-20 18:18:19 +0100

    webSocket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export interface Data {
+    p: number;
+    s: string;
+    t: number;
+    v: number;
+}
+
 export default function Home() {
-    return <div>Hello Trump</div>;
+    const [data, setData] = useState<Data[]>([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            let tsla: Data[];
+
+            const socket = new WebSocket(
+                "wss://ws.finnhub.io?token=cve3031r01qujfbq6q30cve3031r01qujfbq6q3g"
+            );
+
+            // Connection opened -> Subscribe
+            socket.addEventListener("open", function (event) {
+                socket.send(
+                    JSON.stringify({ type: "subscribe", symbol: "TSLA" })
+                );
+            });
+
+            // Listen for messages
+            socket.addEventListener("message", (event) => {
+                tsla = JSON.parse(event.data).data;
+                if (tsla) setData((prevData) => [...prevData, ...tsla]);
+            });
+        };
+
+        getData();
+    }, []);
+
+    return (
+        <div>
+            {data.map((el, idx) => (
+                <div key={idx}>
+                    <p>Symbol: {el.s}</p>
+                    <p>Price: {el.p}</p>
+                    <p>Time: {Math.floor(el.t/1000/60)}</p>
+                    <p>Volume: {el.v}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
