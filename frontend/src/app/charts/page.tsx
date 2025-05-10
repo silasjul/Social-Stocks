967d177c23a152bd4a7c15e542ba07bf7f2fa01a
silasjul <silaskierstein@gmail.com>
2025-05-10 23:21:30 +0200

    sdawd

--- frontend/src/app/charts/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import React from "react";
-
-export default function Charts() {
-    return <AppSidebar activepage="Charts"></AppSidebar>;
-}
--- frontend/src/components/quote-chart.tsx
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import React from "react";
-
-export default async function QuoteChart() {
-    
-    return <div>quote-chart</div>;
-}
